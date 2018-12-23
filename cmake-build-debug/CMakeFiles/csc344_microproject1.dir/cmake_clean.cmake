6de1557e6e043b843929252f095c7398c84eaff7
jtetra13 <jtetra13@gmail.com>
2018-12-23 14:17:11 -0500

    initial commit

---
+++ cmake-build-debug/CMakeFiles/csc344_microproject1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csc344_microproject1.dir/main.c.o"
+  "csc344_microproject1.pdb"
+  "csc344_microproject1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/csc344_microproject1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/csc344_microproject1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for csc344_microproject1.
+# This may be replaced when dependencies are built.
