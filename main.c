abf77a1c7d4e9a01d8526b7536ab6458df8516bd
jtetra13 <jtetra13@gmail.com>
2019-02-16 21:19:39 -0500

    commit after rebasing

--- main.c
+++ main.c
@@ -1,42 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
 
-#define INPUTSIZE 1
-/*struct Node {
+#define NAMESIZE 99
+
+typedef struct {
     char *name;
-    int quantity;
-    struct Node *next;
-    struct Node *prev;
-    struct Node *below;
-    struct Node *above;
-};
+    double price;
+} item;
 
-
-void insert(char *n, int q, struct Node **headRef) {
-    struct Node *newNode = (struct Node *) malloc(sizeof(struct Node));
-    newNode -> name = n;
-    newNode -> quantity = q;
-    if ((*headRef)->prev == NULL && (*headRef)->next == NULL && (*headRef)->below == NULL && (*headRef) -> above == NULL)  {
-        *headRef = newNode;
-    }
-}
-
-struct Node search(char name){
-
-}*/
-
-//char *line
 int main() {
 
-    char *input = malloc(sizeof(char) * 6);
-    printf("%s", "WELCOME TO JUSTIN'S SKIP LIST!\n\nWhat do you wish to do? ");
-    fgets(input, 7, stdin);
-    if (strcmp(input, "insert") == 0) {
-        printf("%s", "What is the item? ");
-        while (!feof(stdin))    {
-            printf(stdin);
+    int shelf_count;
+    int slot_count;
+    int struct_row;
+    int struct_column;
+    int row_inventory;
+    int column_inventory;
+    char answer = 'y';
+    int pass = 0;
+    printf("%s", "How many shelves in the unit? ");
+    scanf(" %d", &shelf_count);
+    item **unit = NULL;
+    unit = malloc(sizeof(item *) * shelf_count);
+    printf("%s", "How many slots are available? ");
+    scanf(" %d", &slot_count);
+    for (int i = 0; i < shelf_count; i++)   {
+        unit[i] = malloc(slot_count * sizeof(item));
+    }
+    printf("%s", "Do you wish to insert an item? (y or n) ");
+    scanf(" %c", &answer);
+    while (answer == 'y') {
+        while (pass == 0) {
+            printf("%s", "What is the item's location, starting with 0? (row)-(column) ");
+            scanf(" %d%*c%d", &struct_row, &struct_column);
+            if (struct_row > shelf_count - 1 || struct_column > slot_count - 1) {
+                printf("%s\n", "These coordinates do not exist.");
+            } else {
+                pass = 1;
+            }
         }
+        printf("%s", "What is the item name? ");
+        (unit[struct_row][struct_column]).name = malloc(NAMESIZE);
+        scanf(" %s", (unit[struct_row][struct_column]).name);
+        printf("%s", "What is the price of this item? ");
+        scanf(" %lf", &(unit[struct_row][struct_column]).price);
+        printf("%s", "Add another item? (y or n) ");
+        scanf(" %c", &answer);
+        pass = 0;
+    }
+    printf("%s", "Search for an item? (y or n) ");
+    scanf(" %c", &answer);
+    while (answer == 'y') {
+        while (pass == 0) {
+            printf("%s", "Enter coordinate of item, starting with 0: (row)-(column) ");
+            scanf(" %d%*c%d", &row_inventory, &column_inventory);
+            if (row_inventory > shelf_count - 1 || column_inventory > slot_count - 1) {
+                printf("%s\n", "These coordinates do not exist.");
+            } else {
+                pass = 1;
+            }
+        }
+        if ((unit[row_inventory][column_inventory].name) != NULL) {
+            printf("%s%s\n%s%.2lf\n", "Name: ", unit[row_inventory][column_inventory].name, "Price: ",
+                   unit[row_inventory][column_inventory].price);
+        } else {
+            printf("%s\n", "No item in this coordinate!");
+        }
+        printf("%s", "Check another coordinate? (y or n) ");
+        scanf(" %c", &answer);
+        pass = 0;
     }
 }
